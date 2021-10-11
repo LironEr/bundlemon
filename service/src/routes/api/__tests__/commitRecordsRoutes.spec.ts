35951c55b5dff89ffd1038a70abd457c6d017988
LironEr <lironerm@gmail.com>
2021-10-11 21:22:27 +0300

    chore(tests): add more tests to server (#50)

--- service/src/routes/api/__tests__/commitRecordsRoutes.spec.ts
+++ service/src/routes/api/__tests__/commitRecordsRoutes.spec.ts
@@ -3,10 +3,33 @@ import { CommitRecordPayload, Compression, CreateCommitRecordResponse } from 'bu
 import { app } from '@tests/app';
 import { createTestProject } from '@tests/projectUtils';
 import { generateRandomString } from '@tests/utils';
-import { getCommitRecordsCollection } from '../../../framework/mongo';
+import { createCommitRecord, getCommitRecordsCollection } from '../../../framework/mongo';
 
 describe('commit records routes', () => {
   describe('create commit record', () => {
+    test('not authenticated', async () => {
+      const { projectId } = await createTestProject();
+
+      const payload: CommitRecordPayload = {
+        branch: 'test',
+        commitSha: generateRandomString(8),
+        files: [{ path: 'file.js', pattern: '*.js', size: 100, compression: Compression.None }],
+        groups: [],
+      };
+
+      const response = await app.inject({
+        method: 'POST',
+        url: `/v1/projects/${projectId}/commit-records`,
+        payload,
+        headers: {
+          'bundlemon-auth-type': 'API_KEY',
+          'x-api-key': 'api-key',
+        },
+      });
+
+      expect(response.statusCode).toEqual(403);
+    });
+
     test('without base branch', async () => {
       const { projectId, apiKey } = await createTestProject();
 
@@ -30,7 +53,7 @@ describe('commit records routes', () => {
       expect(response.statusCode).toEqual(200);
 
       const responseJson = response.json<CreateCommitRecordResponse>();
-      const { record } = responseJson;
+      const { record, baseRecord, linkToReport } = responseJson;
 
       expect(record).toEqual({
         ...payload,
@@ -38,12 +61,75 @@ describe('commit records routes', () => {
         id: record.id,
         creationDate: record.creationDate,
       });
+      expect(baseRecord).toBeUndefined();
+      expect(linkToReport).toEqual('');
 
       // Validate the record exist in the DB
       const commitRecordsCollection = await getCommitRecordsCollection();
       const recordInDb = await commitRecordsCollection.findOne({ _id: new ObjectId(record.id) });
 
       expect(recordInDb).toBeDefined();
+    });
+
+    describe('with base branch', () => {
+      test.each([
+        { name: 'base branch has commit records', baseBranch: 'main' },
+        { name: 'base branch not found', baseBranch: 'new' },
+      ])('$name', async ({ baseBranch }) => {
+        const { projectId, apiKey } = await createTestProject();
+
+        await createCommitRecord(projectId, {
+          branch: 'main',
+          commitSha: generateRandomString(8),
+          files: [{ path: 'file.js', pattern: '*.js', size: 100, compression: Compression.None }],
+          groups: [],
+        });
+
+        const commitRecord2 = await createCommitRecord(projectId, {
+          branch: 'main',
+          commitSha: generateRandomString(8),
+          files: [{ path: 'file.js', pattern: '*.js', size: 100, compression: Compression.None }],
+          groups: [],
+        });
+
+        const payload: CommitRecordPayload = {
+          branch: 'test',
+          baseBranch,
+          commitSha: generateRandomString(8),
+          files: [{ path: 'file.js', pattern: '*.js', size: 100, compression: Compression.None }],
+          groups: [],
+        };
+
+        const response = await app.inject({
+          method: 'POST',
+          url: `/v1/projects/${projectId}/commit-records`,
+          payload,
+          headers: {
+            'bundlemon-auth-type': 'API_KEY',
+            'x-api-key': apiKey,
+          },
+        });
+
+        expect(response.statusCode).toEqual(200);
+
+        const responseJson = response.json<CreateCommitRecordResponse>();
+        const { record, baseRecord, linkToReport } = responseJson;
+
+        expect(record).toEqual({
+          ...payload,
+          projectId,
+          id: record.id,
+          creationDate: record.creationDate,
+        });
+        expect(baseRecord).toEqual(baseBranch === 'main' ? commitRecord2 : undefined);
+        expect(linkToReport).toEqual('');
+
+        // Validate the record exist in the DB
+        const commitRecordsCollection = await getCommitRecordsCollection();
+        const recordInDb = await commitRecordsCollection.findOne({ _id: new ObjectId(record.id) });
+
+        expect(recordInDb).toBeDefined();
+      });
     });
   });
 });
