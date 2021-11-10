9054d7a468df73b0f858eb6e102a7537fd9be0de
shwarcu <emesz@protonmail.com>
2021-11-10 21:20:34 +0100

    custom ouput poc

---
+++ packages/bundlemon/src/main/outputs/outputs/custom.ts
@@ -0,0 +1,50 @@
+import * as yup from 'yup';
+import path from 'path';
+import { Report } from 'bundlemon-utils';
+import { createLogger } from '../../../common/logger';
+import { validateYup } from '../../utils/validationUtils';
+import type { Output, OutputInstance } from '../types';
+
+const NAME = 'custom';
+
+const logger = createLogger(`${NAME} output`);
+
+interface CustomOutputOptions {
+  path?: string;
+}
+interface NormalizedCustomOutputOptions {
+  path: string;
+}
+
+export type CustomOutputFunction = (results: Report) => any;
+
+function validateOptions(options: unknown): NormalizedCustomOutputOptions {
+  const schema: yup.SchemaOf<CustomOutputOptions, CustomOutputOptions> = yup.object().required().shape({
+    path: yup.string().required(),
+  });
+
+  const normalizedOptions = validateYup(schema, options, `${NAME} output`);
+  if (normalizedOptions === undefined) {
+    throw new Error(`validation error in output "${NAME}" options`);
+  }
+  return normalizedOptions as NormalizedCustomOutputOptions;
+}
+
+const output: Output = {
+  name: NAME,
+  create: ({ options }): OutputInstance | Promise<OutputInstance | undefined> | undefined => {
+    const normalizedOptions = validateOptions(options);
+
+    return {
+      generate: async (report: Report): Promise<void> => {
+        const resolvedPath = path.join(process.cwd(), normalizedOptions.path);
+        logger.debug(`Requiring ${resolvedPath}`);
+        // eslint-disable-next-line @typescript-eslint/no-var-requires
+        const customOutput = require(resolvedPath);
+        customOutput(report);
+      },
+    };
+  },
+};
+
+export default output;
--- packages/bundlemon/src/main/outputs/outputs/index.ts
+++ packages/bundlemon/src/main/outputs/outputs/index.ts
@@ -2,9 +2,10 @@
 import consoleOutput from './console';
 import githubOutput from './github';
 import jsonOutput from './json';
+import customOutput from './custom';
 
 import type { Output } from '../types';
 
-const outputs: Output[] = [consoleOutput, githubOutput, jsonOutput];
+const outputs: Output[] = [consoleOutput, githubOutput, jsonOutput, customOutput];
 
 export const getAllOutputs = (): Output[] => outputs;
