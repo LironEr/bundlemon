193b55f2e3ccab1ad07fb845a9aa6c4947a75f60
LironEr <lironerm@gmail.com>
2023-04-26 18:33:05 +0300

    fix high memory

--- jest.config.base.ts
+++ jest.config.base.ts
@@ -5,6 +5,9 @@ const config: Config.InitialOptions = {
   testEnvironment: 'node',
   verbose: true,
   maxWorkers: 1,
+  logHeapUsage: true,
+  // helps with high heap memory https://github.com/jestjs/jest/issues/11956
+  workerIdleMemoryLimit: '1.5G',
 };
 
 export default config;
