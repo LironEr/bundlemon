94ba488e22179b679b6071a932cc851061332098
LironEr <lironerm@gmail.com>
2023-04-26 19:07:38 +0300

    try fix high memory in CI

--- jest.config.base.ts
+++ jest.config.base.ts
@@ -4,7 +4,7 @@ const config: Config.InitialOptions = {
   preset: 'ts-jest',
   testEnvironment: 'node',
   verbose: true,
-  maxWorkers: 1,
+  maxWorkers: '50%',
   logHeapUsage: true,
   // helps with high heap memory https://github.com/jestjs/jest/issues/11956
   workerIdleMemoryLimit: '512MB',
