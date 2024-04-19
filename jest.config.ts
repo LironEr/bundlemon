c4d102c2e81a2729bc3005a32febf64bd0dbe394
LironEr <lironerm@gmail.com>
2024-04-20 00:52:26 +0300

    setup nx

--- jest.config.ts
+++ jest.config.ts
@@ -1,10 +1,15 @@
-import base from './jest.config.base';
-
+import { getJestProjects } from '@nx/jest';
 import type { Config } from '@jest/types';
 
 const config: Config.InitialOptions = {
-  ...base,
-  projects: ['<rootDir>/packages/*/jest.config.ts'],
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  verbose: true,
+  maxWorkers: 1,
+  logHeapUsage: true,
+  // helps with high heap memory https://github.com/jestjs/jest/issues/11956
+  workerIdleMemoryLimit: '1024MB',
+  projects: getJestProjects(),
   collectCoverage: true,
   coverageDirectory: '<rootDir>/coverage/',
   collectCoverageFrom: [`<rootDir>/packages/*/src/**/*.ts`, `!**/__tests__/**`],
--- packages/bundlemon/jest.config.ts
+++
@@ -1,6 +0,0 @@
-import { getJestConfig } from '../../dev-utils/getJestConfig';
-import type { Config } from '@jest/types';
-
-const config: Config.InitialOptions = getJestConfig(__dirname);
-
-export default config;
