62c27caf2c0f341fea29d1615d859749aec82df4
LironEr <lironerm@gmail.com>
2024-04-23 22:43:40 +0300

    WIP

--- apps/service/jest.config.ts
+++ apps/service/jest.config.ts
@@ -2,4 +2,5 @@ export default {
   displayName: 'service',
   preset: '../../jest.preset.js',
   coverageDirectory: '../../coverage/apps/service',
+  setupFilesAfterEnv: ['<rootDir>/tests/hooks.ts'],
 };
